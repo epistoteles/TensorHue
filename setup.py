3eb912cc8a7bb772d808454b0591db556c4f3b8d
Korbinian Koch <korbinian-koch@web.de>
2024-06-03 15:00:27 +0200

    update config

--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
 from setuptools import setup, find_packages
 from tensorhue.version import VERSION
 
-DESCRIPTION = ''
-LONG_DESCRIPTION = ''
+DESCRIPTION = ""
+LONG_DESCRIPTION = ""
 
-setup(
-    name="tensorhue",
-    version=VERSION,
-    author="epistoteles",
-    author_email="<korbinian.koch@uni-hamburg.de>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=[],
-    keywords=[],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+setup(version=VERSION)
