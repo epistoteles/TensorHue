ea7ea45639687ec51782db3c76a78cf71c7fdb37
Korbinian Koch <korbinian-koch@web.de>
2024-06-04 14:12:36 +0200

    single-source version in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup, find_packages
-from tensorhue.version import VERSION
 
-DESCRIPTION = ""
-LONG_DESCRIPTION = ""
-
-setup(version=VERSION)
+setup()
--- tensorhue/__init__.py
+++ tensorhue/__init__.py
@@ -1,10 +1,9 @@
 import sys
 from rich.console import Console
 import numpy as np
-from tensorhue.version import VERSION
 from tensorhue.colors import COLORS
 
-__version__ = VERSION
+__version__ = "0.0.2"  # single source of version truth
 
 __all__ = []
 
@@ -50,12 +49,43 @@ def _viz_tensor(self, colors: tuple[tuple[int], tuple[int]] = None) -> None:
                 f"[rgb({color[y, x, 0]},{color[y, x, 1]},{color[y, x, 2]}) on rgb({bgcolor[y, x, 0]},{bgcolor[y, x, 1]},{bgcolor[y, x, 2]})]▀[/]"
             )
         result_parts.append("\n")
+    if shape[0] % 2 == 1:
+        for x in range(shape[1]):
+            result_parts.append(f"[rgb({color[-1, x, 0]},{color[-1, x, 1]},{color[-1, x, 2]})]▀[/]")
 
     c = Console(log_path=False, record=False)
     c.print("".join(result_parts))
+    return "".join(result_parts)
 
 
 # automagically set up TensorHue
 if "torch" in sys.modules:
     torch = sys.modules["torch"]
     setattr(torch.Tensor, "viz", viz)
+
+
+# def _viz_tensor_alt(self, colors: tuple[tuple[int], tuple[int]] = None) -> None:
+#     if colors is None:
+#         colors = COLORS["default_dark"], COLORS["default_bright"]
+#     data = self.data.numpy()
+#     shape = data.shape
+#     dim = data.ndim
+#     color_a = np.array(colors[0])
+#     color_b = np.array(colors[1])
+#     color = ((1 - data[::2, :, None]) * color_a + data[::2, :, None] * color_b).astype(int)
+#     bgcolor = ((1 - data[1::2, :, None]) * color_a + data[1::2, :, None] * color_b).astype(int)
+
+#     result_parts = []
+#     for y in range(shape[0] // 2):
+#         for x in range(shape[1]):
+#             result_parts.append(
+#                 f"[rgb({color[y, x, 0]},{color[y, x, 1]},{color[y, x, 2]}) on rgb({bgcolor[y, x, 0]},{bgcolor[y, x, 1]},{bgcolor[y, x, 2]})]▀[/]"
+#             )
+#         result_parts.append("\n")
+#     if shape[0] % 2 == 1:
+#         for x in range(shape[1]):
+#             result_parts.append(f"[rgb({color[-1, x, 0]},{color[-1, x, 1]},{color[-1, x, 2]})]▀[/]")
+
+#     c = Console(log_path=False, record=False)
+#     c.print("".join(result_parts))
+#     # return "".join(result_parts)
