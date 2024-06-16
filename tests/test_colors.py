2f9a6ddea24c3a11f52baf8fc2f603bc05f15274
Korbinian Koch <korbinian-koch@web.de>
2024-06-16 22:30:47 +0200

    update tests

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -2,7 +2,9 @@ import pytest
 from rich.color_triplet import ColorTriplet
 import numpy as np
 
-from tensorhue.colors import ColorGradient, ColorScheme, COLORS
+from tensorhue.colors import ColorScheme, COLORS
+from matplotlib.colors import Colormap
+from matplotlib import colormaps
 
 
 def test_COLORS():
@@ -11,20 +13,31 @@ def test_COLORS():
         assert isinstance(value, ColorTriplet)
 
 
-# def test_ColorScheme():
-#     cs = ColorScheme(
-#         gradient=ColorGradient(
-#             [(0.0, ColorTriplet(0, 0, 0)), (0.5, ColorTriplet(255, 255, 255)), (1.0, ColorTriplet(0, 0, 0))]
-#         )
-#     )
-#     assert cs.gradient.gradient == [
-#         (0.0, ColorTriplet(0, 0, 0)),
-#         (0.5, ColorTriplet(255, 255, 255)),
-#         (1.0, ColorTriplet(0, 0, 0)),
-#     ]
-#     assert cs.masked_color == COLORS["masked"]
-#     assert cs.true_color == COLORS["true"]
+def test_ColorScheme():
+    cs = ColorScheme()
 
-#     values = np.array([0.0, 0.5, 0.75])
-#     result = cs.calculate_gradient_color_vectorized(values)
-#     assert np.array_equal(result, np.array([[0, 255, 127], [0, 255, 127], [0, 255, 127]]))
+    assert isinstance(cs.colormap, Colormap)
+    assert cs.masked_color == COLORS["masked"]
+    assert cs.true_color == COLORS["true"]
+    assert np.array_equal(cs.colormap.get_bad()[:3], np.array(cs.masked_color.normalized))
+
+    values1 = np.array([-0.5, 0.0, 0.5, 0.75])
+    result1 = cs(values1)
+    values2 = values1 * 2
+    result2 = cs(values2)
+    assert np.array_equal(result1, result2)
+
+    cs.colormap = colormaps["cividis"]
+    assert np.array_equal(cs.colormap.get_bad()[:3], np.array(cs.masked_color.normalized))
+
+    cs.masked_color = COLORS["black"]
+    assert np.array_equal(cs.colormap.get_bad()[:3], np.array([0, 0, 0]))
+
+    cs.inf_color = COLORS["black"]
+    assert np.array_equal(cs.colormap.get_over()[:3], np.array([0, 0, 0]))
+
+    cs.ninf_color = COLORS["black"]
+    assert np.array_equal(cs.colormap.get_under()[:3], np.array([0, 0, 0]))
+
+    bool_array = np.array([True, False])
+    assert np.array_equal(cs(bool_array), np.array([cs.true_color, cs.false_color]))
