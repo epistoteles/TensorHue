d181696a29b177e2f347ed54c4f0d241b73568e7
Korbinian Koch <korbinian-koch@web.de>
2024-06-18 21:46:31 +0200

    implement JAX functionality and tests

--- tensorhue/__init__.py
+++ tensorhue/__init__.py
@@ -5,6 +5,7 @@ from tensorhue.colors import COLORS, ColorScheme
 from tensorhue._print_opts import PRINT_OPTS, set_printoptions
 from tensorhue._numpy import NumpyArrayWrapper
 from tensorhue._torch import _tensorhue_to_numpy_torch
+from tensorhue._jax import _tensorhue_to_numpy_jax
 from tensorhue.eastereggs import pride
 from tensorhue.viz import viz, _viz
 
@@ -19,3 +20,10 @@ if "torch" in sys.modules:
     torch = sys.modules["torch"]
     setattr(torch.Tensor, "viz", _viz)
     setattr(torch.Tensor, "_tensorhue_to_numpy", _tensorhue_to_numpy_torch)
+if "jax" in sys.modules:
+    jax = sys.modules["jax"]
+    setattr(jax.Array, "viz", _viz)
+    setattr(jax.Array, "_tensorhue_to_numpy", _tensorhue_to_numpy_jax)
+    jaxlib = sys.modules["jaxlib"]
+    setattr(jaxlib.xla_extension.DeviceArrayBase, "viz", _viz)
+    setattr(jaxlib.xla_extension.DeviceArrayBase, "_tensorhue_to_numpy", _tensorhue_to_numpy_jax)
