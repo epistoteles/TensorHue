c613805fdacacfca6124392872d08dd1fdab316a
Korbinian Koch <korbinian-koch@web.de>
2024-06-21 23:57:16 +0200

    rename input to tensor

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -5,49 +5,49 @@ from tensorhue.viz import viz
 from tensorhue._torch import _tensorhue_to_numpy_torch
 
 
-@pytest.mark.parametrize("input", [np.ones(10), _tensorhue_to_numpy_torch(torch.ones(10))])
-def test_1d_tensor(input, capsys):
-    viz(input)
+@pytest.mark.parametrize("tensor", [np.ones(10), _tensorhue_to_numpy_torch(torch.ones(10))])
+def test_1d_tensor(tensor, capsys):
+    viz(tensor)
     captured = capsys.readouterr()
     out = captured.out.rstrip("\n")
     assert len(out.split("\n")) == 2
     assert out.count("▀") == 10
-    assert out.split("\n")[-1] == f"shape = {input.shape}"
+    assert out.split("\n")[-1] == f"shape = {tensor.shape}"
 
 
-@pytest.mark.parametrize("input", [np.ones((10, 10)), _tensorhue_to_numpy_torch(torch.ones(10, 10))])
-def test_2d_tensor(input, capsys):
-    viz(input)
+@pytest.mark.parametrize("tensor", [np.ones((10, 10)), _tensorhue_to_numpy_torch(torch.ones(10, 10))])
+def test_2d_tensor(tensor, capsys):
+    viz(tensor)
     captured = capsys.readouterr()
     out = captured.out.rstrip("\n")
     assert len(out.split("\n")) == 6
     assert out.count("▀") == 100 / 2
-    assert out.split("\n")[-1] == f"shape = {input.shape}"
+    assert out.split("\n")[-1] == f"shape = {tensor.shape}"
 
 
-@pytest.mark.parametrize("input", [np.ones(200), _tensorhue_to_numpy_torch(torch.ones(200))])
-def test_1d_tensor_too_wide(input, capsys):
-    viz(input)
+@pytest.mark.parametrize("tensor", [np.ones(200), _tensorhue_to_numpy_torch(torch.ones(200))])
+def test_1d_tensor_too_wide(tensor, capsys):
+    viz(tensor)
     captured = capsys.readouterr()
     out = captured.out.rstrip("\n")
     assert out.count(" ··· ") == 1
     assert out.count("▀") == 95
-    assert out.split("\n")[-1] == f"shape = {input.shape}"
+    assert out.split("\n")[-1] == f"shape = {tensor.shape}"
 
 
-@pytest.mark.parametrize("input", [np.ones((10, 200)), _tensorhue_to_numpy_torch(torch.ones(10, 200))])
-def test_2d_tensor_too_wide(input, capsys):
-    viz(input)
+@pytest.mark.parametrize("tensor", [np.ones((10, 200)), _tensorhue_to_numpy_torch(torch.ones(10, 200))])
+def test_2d_tensor_too_wide(tensor, capsys):
+    viz(tensor)
     captured = capsys.readouterr()
     out = captured.out.rstrip("\n")
     assert out.count(" ··· ") == 5
     assert out.count("▀") == 950 / 2
-    assert out.split("\n")[-1] == f"shape = {input.shape}"
+    assert out.split("\n")[-1] == f"shape = {tensor.shape}"
 
 
-@pytest.mark.parametrize("input", [np.ones(10), _tensorhue_to_numpy_torch(torch.ones(10))])
-def test_no_legend(input, capsys):
-    viz(input, legend=False)
+@pytest.mark.parametrize("tensor", [np.ones(10), _tensorhue_to_numpy_torch(torch.ones(10))])
+def test_no_legend(tensor, capsys):
+    viz(tensor, legend=False)
     captured = capsys.readouterr()
     out = captured.out.rstrip("\n")
     assert len(out.split("\n")) == 1
